2b899b3124c1c817d82a9cad91381e7b77de02ff
zibro <jgh6272@naver.com>
2022-10-30 17:09:43 +0900

    22/10/30

--- app/src/main/java/com/zibro/gyeonggilocalcurrency/GyeonggiLocalCurrencyApp.kt
+++ app/src/main/java/com/zibro/gyeonggilocalcurrency/GyeonggiLocalCurrencyApp.kt
@@ -1,15 +1,24 @@
 package com.zibro.gyeonggilocalcurrency
 
 import android.app.Application
-import org.koin.android.BuildConfig
+import com.zibro.presentation.CustomTimberTree
 import timber.log.Timber
 
 class GyeonggiLocalCurrencyApp : Application() {
+    companion object {
+        var instance : GyeonggiLocalCurrencyApp? = null
+    }
 
     override fun onCreate() {
         super.onCreate()
+        instance = this
         if(BuildConfig.DEBUG){
-            Timber.plant(com.zibro.presentation.CustomTimberTree())
+            Timber.plant(CustomTimberTree())
         }
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        instance = null
     }
 }
--- data/src/main/java/com/zibro/data/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.zibro.data
-
-class MyClass {
-}
